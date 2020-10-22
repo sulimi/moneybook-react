a06b6ad45398144a762a9e40c5fc061ade908b43
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-22 23:14:35 +0800

    记账页标签css

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -4,7 +4,22 @@ import styled from 'styled-components';
 
 
 const TagsSection=styled.section`
-  
+  background: #fff; padding: 12px 16px;
+    ol{
+      margin: 0 -12px;
+      li{
+        background: #D9D9D9;border-radius: 18px;
+        display: inline-flex;
+        padding: 3px 18px;
+        font-size: 14px;
+        margin: 3px 12px;
+      }
+  }
+  button{
+    border-bottom: 1px solid #ddd;
+    padding: 2px 4px;
+    color: #666;margin-top:8px;
+  }
 `
 const NotesSection=styled.section`
   
@@ -15,6 +30,7 @@ const CategorySection=styled.section`
 const NumberPadSection=styled.section`
   
 `
+
 function Money() {
   return (
     <Layout>
@@ -25,6 +41,7 @@ function Money() {
           <li>住</li>
           <li>行</li>
         </ol>
+        <button>添加标签</button>
       </TagsSection>
       <NotesSection>
         <label>
