68d6bfe7e7c2dca975d525eac8f4bc491d8e9df5
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-23 20:00:19 +0800

    标签页布局，加icon

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -1,14 +1,29 @@
 import Layout from '../components/Layout';
 import React from 'react';
 import {useTags} from '../useTags';
+import styled from 'styled-components';
+import Icon from '../components/Icon';
 
+
+const TagList=styled.ol`
+   font-size: 16px; 
+   > li{
+    border: 1px solid #d5d5d9; line-height: 20px;padding: 12px 16px;background: #fff;display: flex;justify-content: space-between;align-items: center;
+    .icon{
+      width: .7em;height: .7em;
+    }
+   }
+`
 function Tags() {
   const {tags, setTags} = useTags();
   return (
     <Layout>
-      <ol>
-        {tags.map(tag=><li key={tag}>{tag}</li>)}
-      </ol>
+      <TagList>
+        {tags.map(tag=><li key={tag}>
+          <span>{tag}</span>
+          <Icon name='right' />
+        </li>)}
+      </TagList>
     </Layout>
   );
 }
