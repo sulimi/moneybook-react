6f440fbe60ae1f2c629f73dabe4cac262c45610b
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-23 21:45:37 +0800

    exact精确匹配路由

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -3,15 +3,19 @@ import React from 'react';
 import {useTags} from '../useTags';
 import styled from 'styled-components';
 import Icon from '../components/Icon';
+import {Link} from 'react-router-dom';
 
 
 const TagList = styled.ol`
    font-size: 16px; 
    background: #fff;
    > li{
-    border-bottom: 1px solid #d5d5d9; line-height: 20px;padding: 12px 0 16px 0;
-    display: flex;justify-content: space-between;align-items: center;
+    border-bottom: 1px solid #d5d5d9; line-height: 20px;
     margin-left: 16px;margin-right: 16px;
+    > a{
+    display: flex;justify-content: space-between;align-items: center;
+    padding: 12px 0 16px 0;
+    }
     .icon{
       width: .7em;height: .7em;flex-shrink: 0;
     }
@@ -30,13 +34,15 @@ const Space = styled.div`
 `;
 
 function Tags() {
-  const {tags, setTags} = useTags();
+  const {tags} = useTags();
   return (
     <Layout>
       <TagList>
         {tags.map(tag => <li key={tag}>
-          <span className='oneLine'>{tag}</span>
-          <Icon name='right'/>
+          <Link to={'/tags/' + tag}>
+            <span className='oneLine'>{tag}</span>
+            <Icon name='right'/>
+          </Link>
         </li>)}
       </TagList>
       <Space/>
---
+++ src/views/TagEdit.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const TagEdit: React.FC=()=>{
+return (
+  <div>hihihihihi</div>
+)
+}
+
+export {TagEdit}
