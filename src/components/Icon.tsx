8b3792f132942f1a30bf4de9aab5006b6f0956a3
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-24 01:07:13 +0800

    重构Icon,name变得可不传，方便顶部做左中右布局时提供一个空的位置

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -10,12 +10,12 @@ try {
 
 
 type Props = {
-  name: string
+  name?: string
 }
 const Icon = (props: Props) => {
   return (
     <svg className="icon">
-      <use xlinkHref={'#' + props.name}/>
+      {props.name ?? <use xlinkHref={'#' + props.name}/>} //方便顶部做左中右布局时提供一个空的位置
     </svg>
   );
 };
