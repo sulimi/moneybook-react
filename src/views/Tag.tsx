fb4055a1aac54ad3d5f06b60c3fcb7512f3334ff
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-23 20:07:17 +0800

    标签页:标签名很长超出打点隐藏

--- src/views/Tag.tsx
+++ src/views/Tag.tsx
@@ -10,7 +10,7 @@ const TagList=styled.ol`
    > li{
     border: 1px solid #d5d5d9; line-height: 20px;padding: 12px 16px;background: #fff;display: flex;justify-content: space-between;align-items: center;
     .icon{
-      width: .7em;height: .7em;
+      width: .7em;height: .7em;flex-shrink: 0;
     }
    }
 `
@@ -20,7 +20,7 @@ function Tags() {
     <Layout>
       <TagList>
         {tags.map(tag=><li key={tag}>
-          <span>{tag}</span>
+          <span className='oneLine'>{tag}很纯洁的开发进度累计发电量降幅劳动纠纷扩大解放绿卡角度来看附近的路口开绿灯解放立刻搭街坊罗迪克了解对方立刻搭街坊李达康书记</span>
           <Icon name='right' />
         </li>)}
       </TagList>
