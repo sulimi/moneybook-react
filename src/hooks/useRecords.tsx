fb37e2af2735def5c7b1be4f1339aaa0f761b35d
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-25 01:50:12 +0800

    重构账单类型

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,19 +1,32 @@
 import {useEffect, useState} from 'react';
 import {useUpdate} from './useUpdate';
 
-type RecordItem = {
+// type RecordItem = {
+//   tagsId: number[]
+//   note: string
+//   category: '+' | '-'
+//   amount: number
+//   createdAt: string
+// }
+// type newRecordItem={
+//   tagsId: number[]
+//   note: string
+//   category: '+' | '-'
+//   amount: number
+// }
+
+//可以简写
+type newRecordItem = {
   tagsId: number[]
   note: string
   category: '+' | '-'
   amount: number
+}
+type RecordItem = newRecordItem & {
   createdAt: string
 }
-type newRecordItem={
-  tagsId: number[]
-  note: string
-  category: '+' | '-'
-  amount: number
-}
+
+
 const useRecords = () => {
   const [records, setRecords] = useState<RecordItem[]>([]);
   useEffect(() => {
@@ -23,7 +36,7 @@ const useRecords = () => {
     window.localStorage.setItem('records', JSON.stringify(records));
   }, [records]);
   const addRecord = (record: newRecordItem) => {
-    const recordAddTime={...record,createdAt:(new Date()).toISOString()}
+    const recordAddTime = {...record, createdAt: (new Date()).toISOString()};
     setRecords([...records, recordAddTime]);
   };
   return {records, addRecord};
