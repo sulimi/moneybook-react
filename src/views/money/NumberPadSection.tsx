a48df649283af31113520ed459517d13a30cd222
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-25 07:04:00 +0800

    解决不能输入.的bug

--- src/views/money/NumberPadSection.tsx
+++ src/views/money/NumberPadSection.tsx
@@ -1,21 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Wrapper} from './numberPad/NumberPadWrapper';
 import {generateOutPut} from './numberPad/generateOutput';
 
 type Props={amount: number, onChange: (amount: number)=>void, onOk?: ()=>void}
 const NumberPadSection: React.FC<Props> = (props) => {
-  // const [output, _setOutput] = useState('0');
-const output=props.amount.toString()
+  const [output, _setOutput] = useState(props.amount.toString());
   const setOutput = (output: string) => {
-  let value
+  let newOutput
     if (output.length > 16) {
-      value = parseFloat(output.slice(0, 16));
+      newOutput = output.slice(0, 16);
     } else if (output.length === 0) {
-      value = 0;
+      newOutput = '0';
     }else {
-      value=parseFloat(output)
+      newOutput=output
     }
-    props.onChange(value)
+    _setOutput(newOutput)
+    props.onChange(parseFloat(newOutput))
   };
   const onClickButtonWrapper = (e: React.MouseEvent) => {
     const text = (e.target as HTMLButtonElement).textContent;
