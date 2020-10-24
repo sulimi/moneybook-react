3d52a2b3bca404c9eb189464c7a7d2d02603256b
susumi <8033078+susumi@user.noreply.gitee.com>
2020-10-24 09:36:52 +0800

    封装input，用...rest一次性写input属性

---
+++ src/components/Input.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Label = styled.label`
+  display: flex;align-items: center;
+    span{ margin-right: 16px;flex-shrink: 0;}
+    input{
+      display: block;height: 72px;
+      flex-grow: 1;border: none;background: none;
+    }
+`;
+type Props = {
+  text: string;
+} & React.InputHTMLAttributes<HTMLInputElement>;
+const Input: React.FC<Props> = (props) => {
+  const {text, children, ...rest} = props;
+  return (
+    <Label>
+      <span>{props.text}</span>
+      <input {...rest}/>
+    </Label>
+  );
+};
+
+export {Input};
