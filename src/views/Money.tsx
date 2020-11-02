66c19c4d8c9d36214f7fa402cc73f60e910c4b27
susumi <8033078+susumi@user.noreply.gitee.com>
2020-11-02 09:57:38 +0800

    展开收起图标

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -1,6 +1,6 @@
 import Layout from '../components/Layout';
 import styled from 'styled-components';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Icon from '../components/Icon';
 import {Link} from 'react-router-dom';
 import {useRecords} from '../hooks/useRecords';
@@ -11,7 +11,7 @@ const MyLayout = styled(Layout)`
   display: flex;
   flex-direction: column;align-items: center;
   > .about{
-   display:flex;flex-direction: column;justify-content: flex-end;align-items: center;
+   display:flex;flex-direction: column;justify-content: flex-end;align-items: center;flex-shrink: 0;
    height: 30vh;font-weight: bold;
     .paytext{
       color: #A5C9C0;text-align: center;
@@ -26,7 +26,7 @@ const MyLayout = styled(Layout)`
   }
   > .add-wrapper{
       height: 20vh;padding-bottom: 30px;
-      display:flex;flex-direction: column;justify-content: flex-end;align-items: center;
+      display:flex;flex-direction: column;justify-content: flex-end;align-items: center;flex-shrink: 0;
     > .addmoney{
   font-size: 16px;font-weight: bold;color: #444444;
     background: #BFD9D3;border: 1px solid #65C6BB;padding: 12px 40px;border-radius: 10px;
@@ -34,18 +34,19 @@ const MyLayout = styled(Layout)`
   }
 
   > .toggle{
-  display: flex;justify-content: center;align-items: center;
-    font-size: 14px;color: #AAAAAA;
+  display: flex;justify-content: center;align-items: center;flex-shrink: 0;
+    font-size: 14px;color: #AAAAAA;padding: 16px;
     .icon{
       fill: #AAAAAA;margin-left: 10px;
     }
+    
   }
   
 `;
-const ThisMonth = styled.div`
+const ThirtyDay = styled.div`
   display: flex;flex-direction: column;align-items: center;
 `;
-const ThisMonthR = styled.div`
+const ThirtyDayList = styled.div`
 display: flex;align-items: center;
 `;
 
@@ -53,6 +54,10 @@ function Money() {
   const {records} = useRecords();
   const recordArr = hashCreate(records).filter(([d, r], i, arr) =>
     dayjs(d) > dayjs(dayjs(arr[0][0]).subtract(29, 'day').format('YYYY-MM-DD')));
+  const [showR, setShowR] = useState(false);
+  const onShowR = () => {
+    setShowR((showR) => !showR);
+  };
   return (
     <MyLayout message='TODAY'>
       <div className='about'>
@@ -63,34 +68,34 @@ function Money() {
       <Link className='add-wrapper' to='/addmoney'>
         <button className='addmoney'>记一笔</button>
       </Link>
-      <div className='toggle'>展示近30天账单 (6)<Icon name='right'/></div>
-      <ThisMonth>
+      <div className='toggle' onClick={onShowR}>展示近30天账单 (6)
+        {showR?<Icon name='xia'/>:<Icon name='right'/>}
+      </div>
+      {showR && <ThirtyDay>
         {recordArr.map(([date, records]) =>
-          <ThisMonth key={date}>
-            <ThisMonthR>
-              <div>{dayjs(records[0].createdAt).isSame(new Date(),'day')?<span>今天</span>:''}<span>{dayjs(records[0].createdAt).format('MM月')}</span><span>{dayjs(records[0].createdAt).format('D')}</span></div>
+          <ThirtyDay key={date}>
+            <ThirtyDayList>
+              <div>{dayjs(records[0].createdAt).isSame(new Date(), 'day') ?
+                <span>今天</span> : ''}<span>{dayjs(records[0].createdAt).format('MM月')}</span><span>{dayjs(records[0].createdAt).format('D')}</span>
+              </div>
               <div>总金额</div>
-            </ThisMonthR>
-            <ThisMonth>
+            </ThirtyDayList>
+            <ThirtyDay>
               {records.map(r =>
-                <ThisMonthR key={r.id}>
+                <ThirtyDayList key={r.id}>
                   <Icon name={r.tag.icon}/>
                   <div>{r.tag.name}</div>
                   <div>{r.note}</div>
                   <div>￥{r.amount}</div>
-                </ThisMonthR>
+                </ThirtyDayList>
               )}
-            </ThisMonth>
-          </ThisMonth>
+            </ThirtyDay>
+          </ThirtyDay>
         )}
-      </ThisMonth>
+      </ThirtyDay>}
+
     </MyLayout>
   );
 }
 
 export default Money;
-
-// <ThisMonth>
-//   <ThisMonthR> <div>{}</div><div>{}</div></ThisMonthR>
-//   <ThisMonthR><Icon name={} /><div>{}</div><div>{}</div></ThisMonthR>
-// </ThisMonth>
