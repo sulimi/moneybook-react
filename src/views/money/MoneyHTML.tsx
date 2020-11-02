1e4ee63e7ef1729060c48f20d350ca94bffaefc3
susumi <8033078+susumi@user.noreply.gitee.com>
2020-11-02 12:17:20 +0800

    重构主页

---
+++ src/views/money/MoneyHTML.tsx
@@ -0,0 +1,92 @@
+import styled from 'styled-components';
+import Layout from '../../components/Layout';
+
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;align-items: center;
+  > .about{
+   display:flex;flex-direction: column;justify-content: flex-end;align-items: center;flex-shrink: 0;
+   height: 30vh;font-weight: bold;
+    .paytext{
+      color: #A5C9C0;text-align: center;
+      font-size: 14px;
+    }
+    .paynum{
+      color: #A5C9C0;font-size: 32px;text-align: center;padding: 10px;word-break: break-all
+    }
+    .in{
+      font-size: 14px;color: #AAAAAA;text-align: center;word-break: break-all
+    }
+  }
+  > .add-wrapper{
+      height: 20vh;padding-bottom: 30px;
+      display:flex;flex-direction: column;justify-content: flex-end;align-items: center;flex-shrink: 0;
+    > .addmoney{
+  font-size: 16px;font-weight: bold;color: #444444;
+    background: #BFD9D3;border: 1px solid #65C6BB;padding: 12px 40px;border-radius: 10px;
+  }
+  }
+
+  > .toggle{
+  display: flex;justify-content: center;align-items: center;flex-shrink: 0;
+    font-size: 14px;color: #AAAAAA;padding: 16px;
+    .icon{
+      fill: #AAAAAA;margin-left: 10px;
+    }
+    
+  }
+  
+`;
+const ThirtyDay = styled.div`
+  display: flex;flex-direction: column;width: 100%;
+`;
+
+const ThirtyDayHeader = styled.div`
+display: flex;align-items: center;justify-content: space-between;width: 100%;padding: 10px 24px 10px 30px;
+
+.day-header{
+  color:#65C6BB;font-size: 12px;background: #fff;flex-shrink: 0;margin-right: 5px;
+  .today{
+     margin-right: 5px;
+  }
+  .day{
+    font-weight: bold;
+    font-size: 16px;margin-left: 5px;
+  }
+}
+.num{
+display: flex;align-items: center;justify-content: flex-end;flex-wrap: wrap;
+  > span{
+    padding-left: 10px;word-break: break-all;
+  }
+}
+`;
+const ThirtyDayList = styled.div`
+display: flex;align-items: center;justify-content: space-between;padding: 16px;background: #F8F8F6;
+margin: 5px 16px;border-radius: 10px;
+  .icon{
+    width: 3em;height: 3em;flex-shrink: 0;
+  }
+  .amount{
+    font-size: 18px;font-weight: bold;flex-shrink: 0;
+    &.zheng{
+      color:#65C6BB
+    }
+  }
+`;
+const DisplayWrapper = styled.div`
+  display: flex;align-items: center;
+`;
+const DisplayWrapper2 = styled.div`
+  display: flex;flex-direction:column;justify-content: center;flex-grow: 1;
+  .name{
+    flex-grow: 1;flex-shrink: 0;padding: 0 3px 3px;
+    height: 2em;display: flex;align-items: flex-end;
+  }
+  .note{
+      font-size: 10px;color: #AAA;padding: 0 3px 5px;height: 1.5em;max-width: 28vw;text-overflow: ellipsis;white-space: nowrap;overflow: hidden;
+  }
+`;
+
+
+export {MyLayout,ThirtyDay,ThirtyDayHeader,ThirtyDayList,DisplayWrapper,DisplayWrapper2}
