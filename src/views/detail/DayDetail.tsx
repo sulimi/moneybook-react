4faa71c901acea82a77bed59b57cbc38aa28fd96
susumi <8033078+susumi@user.noreply.gitee.com>
2020-11-06 17:36:36 +0800

    提取html

--- src/views/detail/DayDetail.tsx
+++ src/views/detail/DayDetail.tsx
@@ -8,55 +8,9 @@ import {useRecords} from '../../hooks/useRecords';
 import dayjs from 'dayjs';
 import {DayDetailList} from './DayDetailList';
 import {thousand} from '../../lib/thousandSeparator';
+import {DayDetailBottom, DayDetailHeader, DayDetailLength, DayDetailMiddle, DayDetailWrapper} from './DayDetailHtml';
 
 
-const Wrapper = styled.div`
-  display: flex;flex-direction: column;
-`;
-const Header = styled.header`
-  display: flex;flex-direction:column;
-`;
-
-const Middle = styled.div`
-  display: flex;justify-content:center;align-items: center;width: 100%;
-   ul{
-   background: #A5C9C0;color: #fff;margin: 3px;
-    li{
-       &.selected{
-      color: #65C6BB;
-      }
-   }
-  }
-`;
-const Bottom = styled.div`
-display: flex;justify-content:space-around;align-items: center;width: 100%;
-padding: 5px 16px;
-.item{
-width: 33.333%;
-  .num{
-  text-overflow: ellipsis;white-space: nowrap;overflow: hidden;
-  font-weight: bold;padding: 5px;text-align: center;font-size: 18px;
-  &.shou{
-    color: #A5C9C0;
-  }
-  &.greed{
-     color: #A5C9C0;
-  }
-  &.red{
-     color: #FA8072;
-  }
-}
-.text{
-  color: #AAA;padding: 5px;text-align: center;font-size: 12px;
-}
-}
-
-`;
-
-const Length = styled.div`
-  color: #AAA;padding: 5px 16px;font-size: 12px;
-  border-top: 1px solid  #F8F8F6;
-`;
 
 
 const DayDetail = () => {
@@ -86,8 +40,8 @@ const DayDetail = () => {
 
   };
   return (
-    <Wrapper>
-      <Header>
+    <DayDetailWrapper>
+      <DayDetailHeader>
         <Topbar>
           <div className='back' onClick={onClickBack}>
             <Icon name='left'/>
@@ -97,11 +51,11 @@ const DayDetail = () => {
           <Link to='/addmoney' className='save'>
             记一笔
           </Link>
-        </Topbar>
-        <Middle>
+        </Topbar>DayDetai
+        <DayDetailMiddle>
           <CategorySection category={cate} onChange={category => onChange(category)}/>
-        </Middle>
-        <Bottom>
+        </DayDetailMiddle>
+        <DayDetailBottom>
           <div className='item'>
             <div className='num shou'>+￥{showNum('+')}</div>
             <div className='text'>总收入</div>
@@ -114,11 +68,11 @@ const DayDetail = () => {
             <div className={lastAmount()>0?'num greed':'num red'}>{lastAmount()>0?'+':'-'}￥{thousand(Math.abs(lastAmount()).toString())}</div>
             <div className='text'>结余</div>
           </div>
-        </Bottom>
-      </Header>
-      <Length>账单({records.length})：</Length>
+        </DayDetailBottom>
+      </DayDetailHeader>
+      <DayDetailLength>账单({records.length})：</DayDetailLength>
       <DayDetailList records={todayTypeRecords}/>
-    </Wrapper>
+    </DayDetailWrapper>
   );
 };
 
---
+++ src/views/detail/DayDetailHtml.tsx
@@ -0,0 +1,50 @@
+import styled from 'styled-components';
+
+const DayDetailWrapper = styled.div`
+  display: flex;flex-direction: column;
+`;
+const DayDetailHeader = styled.header`
+  display: flex;flex-direction:column;
+`;
+
+const DayDetailMiddle = styled.div`
+  display: flex;justify-content:center;align-items: center;width: 100%;
+   ul{
+   background: #A5C9C0;color: #fff;margin: 3px;
+    li{
+       &.selected{
+      color: #65C6BB;
+      }
+   }
+  }
+`;
+const DayDetailBottom = styled.div`
+display: flex;justify-content:space-around;align-items: center;width: 100%;
+padding: 5px 16px;
+.item{
+width: 33.333%;
+  .num{
+  text-overflow: ellipsis;white-space: nowrap;overflow: hidden;
+  font-weight: bold;padding: 5px;text-align: center;font-size: 18px;
+  &.shou{
+    color: #A5C9C0;
+  }
+  &.greed{
+     color: #A5C9C0;
+  }
+  &.red{
+     color: #FA8072;
+  }
+}
+.text{
+  color: #AAA;padding: 5px;text-align: center;font-size: 12px;
+}
+}
+
+`;
+
+const DayDetailLength = styled.div`
+  color: #AAA;padding: 5px 16px;font-size: 12px;
+  border-top: 1px solid  #F8F8F6;
+`;
+export {DayDetailWrapper,DayDetailHeader,DayDetailMiddle,DayDetailBottom,DayDetailLength}
